f61be4ed464e9b16079735090a92f5de167b2cb4
oerdbej <oerdbej@gmail.com>
2022-05-04 21:34:33 +0200

    api call

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
 import chuck from "./chuck.jpg";
 import "./App.css";
+import { useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  //💡 getting the data while using the axios npm install and we store it into a variable. we want
+
+  useEffect(() => {
+    const result = axios.get("https://api.chucknorris.io/jokes/random");
+    console.log(result);
+  }, []);
+
   return (
     <div>
       <div className='container'>
         <div className='row'>
           <div className='col-6'>
             <h1 className='title'>Chuck Norris Quotes</h1>
-            <img src={chuck} alt='here' />
+            <img src={chuck} alt='here are the quotes of the greatest man' />
+          </div>
+          <div className='col-6 searchJokeCol'>
+            <div className='card'>
+              <div className='card-header'>
+                <span>Search for a quote:</span>
+              </div>
+              <div className='card-body'>
+                <input type='text' />
+              </div>
+            </div>
+
+            <div>
+              <button className='btn btn-warning btn-lg'>Generate Joke:</button>
+            </div>
           </div>
         </div>
       </div>
+      <h3 className='subTitle'>Get the joke:</h3>
+      <h4>Chuck Is not afraid of anything</h4>
     </div>
   );
 }
