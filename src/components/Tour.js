0cbafe4273ef201b6deafb719e4efe3497fb159a
Oerd Bej <oerdbej@gmail.com>
2023-04-21 11:29:31 +0200

    conditional subsctring method for the readmore text

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Tour = ({ id, image, info, name, price, removeTour }) => {
+    //boolean logic
+    const [readMore, setReadMore] = useState(false);
+
     return (
         <div>
             <img src={image} alt={name} />
             <span>{price}</span>
             <div>
                 <h5>{name}</h5>
-                <p>{info}</p>
+                {/* display the text only if the readMore is true, substring method to reduce the strings */}
+                <p>{readMore ? info : `${info.substring(0, 200)}...`}</p>
                 <button onClick={() => removeTour(id)}>Remove</button>
             </div>
         </div>
