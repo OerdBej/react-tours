2ddd3f43ae7c259c61f7525a59eed2c86195dbd8
Oerd Bej <oerdbej@gmail.com>
2023-04-21 12:02:41 +0200

    conditional rendering for the readme section

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -11,7 +11,15 @@ const Tour = ({ id, image, info, name, price, removeTour }) => {
             <div>
                 <h5>{name}</h5>
                 {/* display the text only if the readMore is true, substring method to reduce the strings */}
-                <p>{readMore ? info : `${info.substring(0, 200)}...`}</p>
+                <p>
+                    {readMore ? info : `${info.substring(0, 200)}...`}
+                    {/* conditional: button change the value for readMe */}
+                    <button
+                        type='button'
+                        onClick={() => setReadMore(!readMore)}>
+                        {readMore ? 'show less' : 'read more'}
+                    </button>
+                </p>
                 <button onClick={() => removeTour(id)}>Remove</button>
             </div>
         </div>
